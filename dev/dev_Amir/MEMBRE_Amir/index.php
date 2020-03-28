944160939cc36d3d302a4d7346131c99c51e9548
Amirlimayma <58433331+Amirlimayma@users.noreply.github.com>
2020-03-28 10:55:18 +0100

    Add files via upload

---
+++ dev/dev_Amir/MEMBRE_Amir/index.php
@@ -0,0 +1,16 @@
+<?php session_start();
+require 'inc/functions.php'; ?>
+
+<?php  (isset($_SESSION['auth'])) ?>
+
+<?php require  'inc/header.php'; ?>
+
+
+
+<h1>Acceuil</h1>
+
+
+
+
+
+<?php require 'inc/footer.php'; ?>
---
+++ dev/dev_Amir/MEMBRE_Amir/wikicomp.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  sam. 28 mars 2020 à 10:46
+-- Version du serveur :  10.1.37-MariaDB
+-- Version de PHP :  7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `wikicomp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `Laboratoire` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `confirmation_token` varchar(60) DEFAULT NULL,
+  `confirmed_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
